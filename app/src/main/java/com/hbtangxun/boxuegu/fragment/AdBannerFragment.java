6c40f3135a3db048b67404d0b5d96f361054a656
DestingIceFire <destingicefire@163.com>
2020-05-19 16:31:53 +0800

    第七章 课程

---
+++ app/src/main/java/com/hbtangxun/boxuegu/fragment/AdBannerFragment.java
@@ -0,0 +1,6 @@
+package com.hbtangxun.boxuegu.fragment;
+
+import androidx.fragment.app.Fragment;
+
+public class AdBannerFragment extends Fragment {
+}
---
+++ app/src/main/java/com/hbtangxun/boxuegu/view/ViewPagerIndicator.java
@@ -0,0 +1,65 @@
+package com.hbtangxun.boxuegu.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+import com.hbtangxun.boxuegu.R;
+
+/**
+ * 轮播图的小圆点控件
+ */
+public class ViewPagerIndicator extends LinearLayout {
+
+    private int mCount;  //小圆点的个数
+    private int mIndex;  //当前小圆点的位置
+    private Context mContext;
+
+    public ViewPagerIndicator(Context context) {
+        this(context, null);
+    }
+
+    public ViewPagerIndicator(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.mContext = context;
+        setGravity(Gravity.CENTER); //设置此布局居中
+    }
+
+    /**
+     * 设置滑动到当前小数点时其他圆点的位置
+     *
+     * @param currentIndex
+     */
+    public void setCurrentPosition(int currentIndex) {
+        mIndex = currentIndex; //当前小圆点
+        removeAllViews();      //移除界面上存在的View
+        int pex = 5;
+        for (int i = 0; i < mCount; i++) {
+            //创建一个ImageView控件来放置小圆点
+            ImageView imageView = new ImageView(mContext);
+            if (mIndex == i) {
+                imageView.setImageResource(R.drawable.indicator_on);
+            } else {
+                imageView.setImageResource(R.drawable.indicator_off);
+            }
+
+            imageView.setPadding(pex, 0, pex, 0);
+            addView(imageView);
+
+        }
+    }
+
+    /**
+     * 设置小圆点的数目
+     *
+     * @param count
+     */
+    public void setCount(int count) {
+        this.mCount = count;
+    }
+
+}
