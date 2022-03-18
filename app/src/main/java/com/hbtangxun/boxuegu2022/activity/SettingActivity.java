f0bf260e27146ccac2d1639068e271c130793a08
DestingIceFire <destingicefire@163.com>
2022-03-18 10:40:14 +0800

    新建 设置 界面

---
+++ app/src/main/java/com/hbtangxun/boxuegu2022/activity/SettingActivity.java
@@ -0,0 +1,18 @@
+package com.hbtangxun.boxuegu2022.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.hbtangxun.boxuegu2022.R;
+
+/**
+ * 设置 界面
+ */
+public class SettingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
