66d8d6a3e2f3b3cbcadb85e271b5e6516b0cecb7
DestingIceFire <destingicefire@163.com>
2022-03-30 14:39:06 +0800

    习题详情模块 xml界面  完成

---
+++ app/src/main/java/com/hbtangxun/boxuegu2022/activity/ExercisesDetailActivity.java
@@ -0,0 +1,19 @@
+package com.hbtangxun.boxuegu2022.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hbtangxun.boxuegu2022.R;
+
+/**
+ * 习题详情 界面
+ */
+public class ExercisesDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercises_detail);
+    }
+}
