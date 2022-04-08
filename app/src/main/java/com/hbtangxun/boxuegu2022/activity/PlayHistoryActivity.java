c1273cdb225d3ba6f6891d3f3484b9ff68197908
DestingIceFire <destingicefire@163.com>
2022-04-08 13:58:26 +0800

    播放记录--布局搭建

---
+++ app/src/main/java/com/hbtangxun/boxuegu2022/activity/PlayHistoryActivity.java
@@ -0,0 +1,16 @@
+package com.hbtangxun.boxuegu2022.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hbtangxun.boxuegu2022.R;
+
+public class PlayHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_history);
+    }
+}
